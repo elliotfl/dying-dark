c4f60c4638df3b67af6d00b89b862bc7770834e9
Rileymutt <araveg@mynuames.org>
2018-03-08 09:15:16 -0700

    Finished BasicMovement

---
+++ BasicMovement Unity/Assets/AlignedCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AlignedCamera : MonoBehaviour {
+
+	public Transform player; 
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = player.position;
+	}
+}
---
+++ BasicMovement Unity/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+	
+	public Rigidbody rb;
+
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("w")) 
+		{
+			rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+		}
+
+		if (Input.GetKey ("d")) 
+		{
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey ("a")) 
+		{
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey ("s")) 
+		{
+			rb.AddForce (0, 0, -forwardForce * Time.deltaTime);
+		}
+
+
+	}
+}

44ad2ee524203d97791357c80742426f2a6bd991
alaricsmith <36927521+alaricsmith@users.noreply.github.com>
2018-04-13 08:52:30 -0600

    Worked on player camera and body movement
    Incomplete but almost done

--- Maze Scene/Assets/NewBehaviourScript1.cs
+++ Maze Scene/Assets/NewBehaviourScript1.cs
@@ -1,48 +1,35 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class NewBehaviourScript1 : MonoBehaviour {
 
 	public Rigidbody rb;
 
-	public float forwardForce = 2000f;
-	public float sidewaysForce = 500f;
 	public bool ShowCursor;
 	public float Sensitivity;
 
 	// Update is called once per frame
 	void Start()
 	{
+		rb = GetComponent<Rigidbody>();
+
 		if(ShowCursor == false)
 		{
 			Cursor.visible = false;
 		}
 	}
 
-	void Update () 
+	void Update()
 	{
 		float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * Sensitivity;
 		float newRotationX = transform.localEulerAngles.x + Input.GetAxis("Mouse Y") * Sensitivity;
 
 		gameObject.transform.localEulerAngles = new Vector3(newRotationX, newRotationY, 0);
 
-		if (Input.GetKey ("w")) 
-		{
-			rb.AddForce (0, 0, forwardForce * Time.deltaTime);
-		}
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 500.0f;
+		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 10.0f;
 
-		if (Input.GetKey ("d")) 
-		{
-			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0);
-		}
-
-		if (Input.GetKey ("a")) 
-		{
-			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0);
-		}
-
-		if (Input.GetKey ("s")) 
-		{
-			rb.AddForce (0, 0, -forwardForce * Time.deltaTime);
-		}
+		transform.Rotate (0, x, 0);
+		transform.Translate (0, 0, z);
 	}
 }

16980ea0d5681252f821f0e3443bcf9ea82cbe2c
Rileymutt <araveg@mynuames.org>
2018-05-09 09:20:50 -0600

    Added interaction script to main project

---
+++ MG_Maze Scene A1.2/Assets/Interaction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+
+	public int Collection = 0;
+	public GameObject Sphere;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Collection == 4) 
+		{
+			Sphere.SetActive (false);
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Wall_1")
+		{
+			Destroy (col.gameObject);
+			++Collection;
+		}
+
+		if (col.gameObject.name == "Wall_2")
+		{
+			Destroy (col.gameObject);
+			++Collection;
+		}
+
+		if (col.gameObject.name == "Wall_3")
+		{
+			Destroy (col.gameObject);
+			++Collection;
+		}
+
+		if (col.gameObject.name == "Wall_4")
+		{
+			Destroy (col.gameObject);
+			++Collection;
+		}
+	}
+}
